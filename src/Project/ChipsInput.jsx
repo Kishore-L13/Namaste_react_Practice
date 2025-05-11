b01fc82421d50c72a96c43065701ef55ea8d7253
kishore <kishore.krrish13@gmail.com>
2025-05-11 23:12:15 +0530

    ChipsInput

--- src/Project/ChipsInput.jsx
+++ src/Project/ChipsInput.jsx
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
-import './styles.css'
 function ChipsInput() {
+const [input,setInput] = useState("")
+const [chips , setChips] = useState([])
+const [counter,setCounter] = useState(0)
+
+const handlekeypress = (event) => {
+ if (event.key === "Enter" && input.trim()!==""){
+    const item = {
+        id: counter,
+        label: input.trim()
+    }
+    setChips(prev=>[...prev,item]);
+    setCounter(counter+1)
+    setInput("")
+ }
+
+}
+ const handleDeleteChip = (id) =>{
+   setChips (chips.filter((C)=>
+        C.id !== id
+    ))
+ }
 
   return (
     <div style={{display:"flex", flexDirection:"column",alignItems:"center", margin:"40px 0"}}>
@@ -8,8 +28,17 @@ function ChipsInput() {
       <input
         type="text" 
         placeholder="Type a chip and press tag"
+        value={input}
+        onChange={(e)=>setInput(e.target.value)}
+        onKeyDown={handlekeypress}
         style={{ padding: "8px", width: "200px" }}
       />
+      {chips.map((C)=>(
+        <div key={C.id} className="label">
+        <span>{C.label}</span>
+        <button className="label-btn"
+        onClick={()=>handleDeleteChip(C.id)}>X</button>
+      </div>))}
     </div>
   );
 }
