ae3cc66a85d434cdaeb8971ebad655d42d812b0d
kishore <kishore.krrish13@gmail.com>
2025-05-18 00:26:31 +0530

    Search-api

--- src/Project/Search_api.jsx
+++ src/Project/Search_api.jsx
@@ -3,30 +3,40 @@ import React, { useEffect, useState } from 'react'
 const Search_api = () => {
     const [input , setInput] = useState('')
     const [search,setSearch] = useState([])
-
+    const [show,setShow] = useState(false)
+    const [cache , setCache] = useState([])
 useEffect(()=>{
     const timer = setTimeout(()=>{
-      Api_data()
-    },3000)
+    Api_data()
+    },300)
 return () => {
     clearTimeout(timer)
 }
 },[input])
 const Api_data = async() =>{
-    const response =  await fetch('https://dummyjson.com/recipes')
+ // console.log("api"+input)
+  if (cache[input]){
+    //console.log('cache',input)
+    setSearch(cache[input])
+    return
+  }
+    const response =  await fetch('https://dummyjson.com/recipes/search?q='+input)
     const data = await response.json()
     console.log(data)
       setSearch(data?.recipes);
+      setCache(prev => ({...prev, [input]:data?.recipes}))
 }
   return (
     
         <div className='search-api'>
         <input type='text' placeholder='Enter the recipes'
+        onFocus={()=>setShow(true)}
+        onBlur={()=>setShow(false)}
         value={input} onChange={(e)=>setInput(e.target.value)}
         />
-      {search.map(item => (
+      {show && (search.map(item => (
         <span key={item.id}>{item.name}</span>
-     ))} 
+     )))} 
     </div>
   )
 }
