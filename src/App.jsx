208ce7cf15ae0374b56b06893b094abdd6efee71
kishore <kishore.krrish13@gmail.com>
2025-05-17 23:52:20 +0530

    Search-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
 import './App.css'
-import ChipsInput from './Project/ChipsInput'
-import OTP_input from './Project/OTP_input'
-import TodoList from './Project/TodoList'
-import Auto_search from './Project/Auto_search'
 import Search_api from './Project/Search_api'
 function App() {
 
   return (
     <>
       <div>
-        <Auto_search/>
         <Search_api/>
       </div>
     </>
--- src/Project/Search_api.jsx
+++ src/Project/Search_api.jsx
@@ -16,17 +16,16 @@ const Api_data = async() =>{
     const response =  await fetch('https://dummyjson.com/recipes')
     const data = await response.json()
     console.log(data)
-    setSearch(recipes?.name)
+    setSearch(data?.recipes?.name)
 }
   return (
     
         <div>
         <input type='text' placeholder='Enter the recipes'
-        onKeyDown={handlekeypress}
         value={input} onChange={(e)=>setInput(e.target.value)}
         />
       {search.map(item => (
-        <span>{item}</span>
+        <span key={item.id}>{item.name}</span>
      ))} 
     </div>
   )
