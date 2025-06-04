f154e74ed1d111bda12555108b2d26c4369009ce
kishore <kishore.krrish13@gmail.com>
2025-06-04 23:13:57 +0530

    Dark_light

--- src/Project/Dark_light.jsx
+++ src/Project/Dark_light.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dark_light = () => {
+    const [isvisible, setIsvisible] = useState(false)
+    const toggle = ()=>{
+        setIsvisible(!isvisible)
+    }
   return (
-    <div>Dark_light</div>
+    <div style={{backgroundColor:isvisible?"black":"White",color:isvisible?"white":"black",
+        border:"1px solid black",
+    }}>Dark_light
+        <div><button onClick={()=>toggle()}>{isvisible ? "Light":"Dark"}</button></div>
+    </div>
   )
 }
 
