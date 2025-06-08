012ea66ed1efb97abe7cab93a56323ddfabaa229
kishore <kishore.krrish13@gmail.com>
2025-06-08 14:22:35 +0530

    Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import './App.css'
-import Dark_light from './Project/Dark_light'
-import Input_focus from './Project/Input_focus'
-import User_profile from './Project/User_profile'
-import Modelon from './Project/Modelon'
-import Tabel from './Project/Tabel'
-import Stopwatch from './Project/Stopwatch'
-import StarRating from './Project/StarRating'
 import OTP from './Project/OTP'
 import Dice_roller from './Project/Dice_roller'
+import Carousel from './Project/Carousel'
 function App() {
 
   return (
     <>
     <OTP/>
     <Dice_roller/>
-<StarRating/>
+<Carousel/>
 
     </>
   )
