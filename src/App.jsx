54869c12239b1108a9ac76885b727b0b8ece8577
kishore <kishore.krrish13@gmail.com>
2025-06-13 21:55:37 +0530

    PreviousCounter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import "i18next"
 import Timer_disable from './Project/Timer_disable'
+import PreviousCounter from './Project/PreviousCounter'
 
 function App() {
 
   return (
     <>
   <Timer_disable/>
+  <PreviousCounter/>
     </>
   )
 }
--- src/Project/PreviousCounter.jsx
+++ src/Project/PreviousCounter.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import usePrevious from './usePrevious'
 const PreviousCounter = () => {
+    const [count,setCount] = useState(0)
+    const increment = ()=>{
+        setCount(prev=>prev+1)
+    }
+    const decrement = ()=>{
+        setCount(prev=>prev-1)
+    }
+    const reset = ()=>{
+        setCount(0)
+    }
+      const previousCount = usePrevious(count);
   return (
-    <div>PreviousCounter</div>
+    <div>
+        <h1>Counter: {count}</h1>
+        <h2>Previous: {previousCount}</h2>
+        <button onClick={()=>increment()}>Increment</button>
+        <button onClick={()=>reset()}>Reset</button>
+        <button onClick={()=>decrement()}>Decrement</button>
+    </div>
   )
 }
 
