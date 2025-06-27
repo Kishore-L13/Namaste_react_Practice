949c800e19b5d4e00fd38f693b43cfc9260b2e7e
kishore <kishore.krrish13@gmail.com>
2025-06-27 22:53:27 +0530

    Toogle

--- src/Project/Toogle.jsx
+++ src/Project/Toogle.jsx
@@ -1,22 +1,14 @@
 import React, { useState } from 'react';
-import "./styles.css"
 
-// ✅ Complete the custom hook below
 function useToggle(initialValue = false) {
-  // your code here
   const [value, setValue] = useState(initialValue)
   const toggle = () => setValue((prev) => !prev)
   return [value,toggle]
 }
-
 export default function App() {
-  // ✅ Use the custom hook inside this component
   const [isOn, toggle] = useToggle(false);
-
   return (
-    /* your toggle function for the onClick method */
     <button onClick={toggle} data-testid="toggle-button">
-      {/* Render "ON" or "OFF" based on state */}
       {isOn ? "ON" : "OFF"}
     </button>
   );
