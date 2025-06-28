c5e3539bb1271991fdc5146e6f31a837a4f310e1
kishore <kishore.krrish13@gmail.com>
2025-06-28 20:38:25 +0530

    Image_Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "i18next"
 import Timer_disable from './Project/Timer_disable'
 import PreviousCounter from './Project/PreviousCounter'
 import Greeting from './Project/Greeting'
+import Image_Carousel from './Project/Image_Carousel'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
   <Timer_disable/>
   <PreviousCounter/>
   <Greeting/>
+  <Image_Carousel/>
     </>
   )
 }
