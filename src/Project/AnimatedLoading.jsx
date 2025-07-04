674dbf6922bd19e173eb0530d4d60475f4bacd23
kishore <kishore.krrish13@gmail.com>
2025-07-04 20:29:35 +0530

    AnimatedLoading

--- src/Project/AnimatedLoading.jsx
+++ src/Project/AnimatedLoading.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const AnimatedLoading = () => {
+function LoadingSkeleton() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false))
+    return ()=> clearTimeout(timer)
+},[])
   return (
-    <div>AnimatedLoading</div>
-  )
+    <div className="card">
+      {loading ? (
+        <div className="skeleton-wrapper">
+          {/* write code here */}
+          <div className="skeleton line name"></div>
+           <div className="skeleton line bio"></div>
+        </div>
+      ) : (
+        <div className="content">
+          <h2>John Doe</h2>
+          <p>Full-stack developer at XYZ company</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default AnimatedLoading
+export default LoadingSkeleton;
