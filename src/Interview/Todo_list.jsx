004d55da60d164832968d019c11adde1c84e247c
kishore <kishore.krrish13@gmail.com>
2025-07-11 22:20:40 +0530

    TodoList

--- src/Interview/Todo_list.jsx
+++ src/Interview/Todo_list.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Todo_list = () => {
-  return (
-    <div>Todo_list</div>
-  )
+  const [input, setInput] = useState("")
+  const [todo,setTodo] = useState([])
+const Addtn = ()=>{
+  
+  if (input.trim() === "") return;
+  const items = {id:Date.now(),text:input}
+  setTodo([...todo,items])
+setInput("")
+}
+const delettodo = (id)=>{
+  const D = todo.filter((_,i)=> i!== id)
+  setTodo(D)
 }
 
+  const handleChange = (e) => {
+    let value = e.target.value;
+    setInput(value)
+  }
+  return (
+    <div>
+      <input type= 'text'  value={input} onChange={handleChange}/>
+      <button onClick={()=>Addtn()}>Add</button>
+      <ul>
+      {todo.map((todo,index)=>(
+        <li key={index}>{todo.text} 
+        <button onClick={()=>delettodo(index)}>Del</button>
+        </li>
+      ))}
+      </ul>
+      </div>
+  )
+}
 export default Todo_list
