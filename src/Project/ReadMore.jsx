d470e6edafb3f4a868f64bde53f54dfecd7fd4a4
kishore <kishore.krrish13@gmail.com>
2025-07-23 22:23:06 +0530

    ReadMore

--- src/Project/ReadMore.jsx
+++ src/Project/ReadMore.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./styles.css";
 
-const ReadMore = () => {
+function ReadMoreToggle() {
+  
+  const text = `React is a popular JavaScript library developed by Facebook for 
+  building user interfaces, especially single-page applications. It allows
+   developers to create reusable UI components that efficiently update and 
+   render as data changes. One of React’s key features is the virtual DOM, 
+   which improves performance by minimizing direct manipulation of the actual
+    DOM.`;
+
+
   return (
-    <div>ReadMore</div>
-  )
+    <div className="readmore-container">
+      <h1 className="title">Read More Toggle</h1>
+      <p className="readmore-text" data-testid="readmore-text">
+        {text}
+      </p>
+      <button
+        className="readmore-button"
+        data-testid="readmore-button"
+      >
+        Read More
+      </button>
+    </div>
+  );
 }
 
-export default ReadMore
+export default ReadMoreToggle;
