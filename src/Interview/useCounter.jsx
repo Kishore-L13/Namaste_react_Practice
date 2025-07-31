df782d604a72b84dd5c42bf97197d51a88036145
kishore <kishore.krrish13@gmail.com>
2025-07-31 21:49:55 +0530

    Button

--- src/Interview/useCounter.jsx
+++ src/Interview/useCounter.jsx
@@ -14,3 +14,22 @@ const useCounter = ()=>{
   }
 }
 export default useCounter;
+
+
+// import React from "react";
+// import "./style.css";
+// import useCounter from './useCounter'
+// import Button from './Button'
+// export default function App() {
+//   const {count,increment,decrement,reset} = useCounter()
+//   return (
+//     <div>
+//       <h1>Hello StackBlitz!</h1>
+//       <p>Start editing to see some magic happen :)</p>
+//       <h2>Count:{count}</h2>
+//       <Button label='INC' onClick={increment}>Inc</Button>
+//       <Button label='DEC' onClick={decrement}>Dec</Button>
+//       <Button label = 'Reset'onClick={reset}>reset</Button>
+//     </div>
+//   );
+// }
