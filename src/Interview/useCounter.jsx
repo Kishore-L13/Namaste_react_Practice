35cd8ac2b0d1084148810ddb6518066415c6d34a
kishore <kishore.krrish13@gmail.com>
2025-07-31 21:47:38 +0530

    useCounter

--- src/Interview/useCounter.jsx
+++ src/Interview/useCounter.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const useCounter = () => {
-  return (
-    <div>useCounter</div>
-  )
+const useCounter = ()=>{
+  const [count,setCount] =useState(0)
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1 )
+  }
+  const reset = () => setCount(0)
+  return {
+    count, increment, decrement, reset,
+  }
 }
-
-export default useCounter
+export default useCounter;
